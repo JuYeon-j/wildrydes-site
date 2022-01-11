b2703515f0f280ca91058269d252e29c9e10668b
JuYeon-j <wndus99227@gmail.com>
2022-01-11 17:14:50 +0900

    풀 수정

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_UV5t47jlQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4a2u7auj0q5n2nllg3d36ki3np', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
